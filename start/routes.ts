3d8f6c677809ec0f6c667f1261b5a02500ff9941
Ali Habibi <ali.habibi281@gmail.com>
2023-09-12 18:07:22 +0330

    install and config pg db and first migration user

--- start/routes.ts
+++ start/routes.ts
@@ -19,6 +19,13 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+})
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
